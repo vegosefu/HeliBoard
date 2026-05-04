8aa5c8472b52991a26d06590fed6acd464e50c1c
vegosefu <ac7577569@gmail.com>
2026-05-04 19:20:28 +0000

    fix: clean InputLogic punctuation check

--- app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
+++ app/src/main/java/helium314/keyboard/latin/inputlogic/InputLogic.java
@@ -452,7 +452,7 @@ public final class InputLogic {
         mJustRevertedACommit = false;
         final Event processedEvent = mWordComposer.processEvent(event);
         if (settingsValues.mHidePunctuationKeys && (processedEvent.getCodePoint() == (int)'.' || processedEvent.getCodePoint() == (int)',')) return;
-        if (settingsValues.mHidePunctuationKeys && (processedEvent.getMCodePoint() == (int)'.' || processedEvent.getMCodePoint() == (int)',')) return;
+        if (settingsValues.mHidePunctuationKeys && (processedEvent.getCodePoint() == (int)'.' || processedEvent.getCodePoint() == (int)',')) return;
         final InputTransaction inputTransaction = new InputTransaction(settingsValues,
                 processedEvent, SystemClock.uptimeMillis(), mSpaceState,
                 getActualCapsMode(settingsValues, keyboardShiftMode));
